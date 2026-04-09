b9df0a1a8d7c55951cdad9fb2415096d5e4e92ff
harriskmu <32658086+harriskmu@users.noreply.github.com>
2026-04-09 11:23:22 +0700

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,17 @@
+# Run this once before opening analysis.Rmd
+
+packages <- c(
+  "faraway",
+  "glmnet",
+  "MASS",
+  "olsrr",
+  "lmtest",
+  "caret",
+  "dplyr",
+  "ggplot2",
+  "reshape2",
+  "knitr",
+  "scales"
+)
+
+install.packages(packages)
